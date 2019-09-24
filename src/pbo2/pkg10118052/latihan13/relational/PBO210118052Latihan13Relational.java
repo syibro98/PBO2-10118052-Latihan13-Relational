b24da3f69e1dc0bb188fd67ee98ecc9e1df76fe7
Bagus Syibro Malisi <55564197+syibro98@users.noreply.github.com>
2019-09-24 12:47:23 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118052/latihan13/relational/PBO210118052Latihan13Relational.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118052.latihan13.relational;
+
+/**
+ * @author 
+ * NAMA      : Bagus Syibro Malisi
+ * KELAS     : PBO2
+ * NIM       : 10118052
+ * Deskripsi Program : latihin ini bertujuan untuk mengetahui operator rational
+ */
+public class PBO210118052Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
